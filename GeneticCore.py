ffb5cfa4c5cca4370d0d97ddd56ea78cc427a6c5
Mohammad H. Nazemi <nazemimohammadh@gmail.com>
2016-09-01 23:47:29 +0430

    First Commit

---
+++ GeneticCore.py
@@ -0,0 +1,266 @@
+from random import randint
+from queue import Queue
+
+class Node:
+    def __init__(self,value,level,leftChildNode,rightChildNode,leaf=False):
+        self.value=value
+        self.level=level
+        self.leftChildNode=leftChildNode
+        self.rightChildNode=rightChildNode
+        self.leaf=leaf
+    def __init__(self,leaf=False):
+        self=self
+        self.leaf=leaf
+
+class Tree:
+    def __init__(self,maineNode):
+        self.mainNode=maineNode
+        self.nodes=[]
+
+class Chromosome:
+    def __init__(self,tree,fitness=0):
+        self.fitness=fitness
+        self.tree=tree
+
+
+def ParseData(fileName):
+    file=open(fileName)
+    array=[]
+    for line in file:
+        innerArray=[]
+        for char in line.rstrip():
+            innerArray.append(char)
+        array.append(innerArray)
+
+    return  array
+
+def CreateRandomPool(sizeOfPool):
+    for i in range(0,sizeOfPool):
+        nodesQueue=Queue()
+        node=Node()
+        node.parent=None
+        node.level=0
+        tree=Tree(node)
+        nodes=tree.nodes
+        nodes.append(node)
+        chromosome=Chromosome(tree)
+        pool.append(chromosome)
+
+        nodesQueue.put(node)
+        while (True):
+            if (nodesQueue.empty()):
+                break
+            else:
+                node=nodesQueue.get()
+                if (node.level<3):
+                    node.value=operators[randint(0,1)]
+                    if (node.value=='&' or node.value=='|'):
+                        tempNode=Node()
+                        tempNode.parent=node
+                        tempNode.level=node.level+1
+                        node.rightChildNode=tempNode
+                        nodesQueue.put(tempNode)
+                        nodes.append(tempNode)
+                        tempNode=Node()
+                        tempNode.parent=node
+                        tempNode.level=node.level+1
+                        node.leftChildNode=tempNode
+                        nodesQueue.put(tempNode)
+                        nodes.append(tempNode)
+                elif (node.level==3):
+                    node.value=operatorsAndOperands[randint(0,6)]
+                    if (node.value=='&' or node.value=='|'):
+                        tempNode=Node()
+                        tempNode.parent=node
+                        tempNode.level=node.level+1
+                        node.rightChildNode=tempNode
+                        nodesQueue.put(tempNode)
+                        nodes.append(tempNode)
+                        tempNode=Node()
+                        tempNode.parent=node
+                        tempNode.level=node.level+1
+                        node.leftChildNode=tempNode
+                        nodesQueue.put(tempNode)
+                        nodes.append(tempNode)
+                    else:
+                        node.leaf=True
+
+                elif  (node.level==4):
+                    node.value=operands[randint(0,4)]
+                    node.leaf=True
+
+def CalculateFittnessOFthePool(valueDict,finallResult):
+    for i in range(0,len(pool)):
+        chromosome=pool[i]
+        tree=chromosome.tree
+        node=tree.mainNode
+        operationStack=[]
+
+        readyStateNode=None
+
+        if(not node.leaf):
+            operationStack.append(node)
+        result=0
+        while(len(operationStack)!=0):
+            node=operationStack.pop()
+            if(node.leaf and readyStateNode != None):
+                operatorNode=operationStack.pop()
+                if (operatorNode=='&'):
+                    result=valueDict[node.value] & valueDict[readyStateNode.value]
+                else:
+                    result=valueDict[node.value] | valueDict[readyStateNode.value]
+                newNode=Node()
+                newNode.value=result
+                newNode.leaf=True
+                operationStack.append(newNode)
+                readyStateNode=None
+            elif (node.leaf and readyStateNode == None):
+                readyStateNode=node
+            elif (not node.leaf and readyStateNode != None):
+                operationStack.append(readyStateNode)
+                readyStateNode=None
+                operationStack.append(node)
+                operationStack.append(node.rightChildNode)
+                operationStack.append(node.leftChildNode)
+            elif (not node.leaf and  readyStateNode == None):
+                operationStack.append(node)
+                operationStack.append(node.rightChildNode)
+                operationStack.append(node.leftChildNode)
+        if (result==finallResult):
+            chromosome.fitness=chromosome.fitness+1
+
+def InitilizePool(sizeOfPool):
+    tempPool=[]
+    totatFitness=0
+    for choromosoe in pool:
+        totatFitness=totatFitness+choromosoe.fitness
+    pool.sort(key=lambda x: x.fitness,reverse=True)
+    index=0
+    while(len(tempPool)!=sizeOfPool):
+        choromosoe=pool[index]
+        index = index +1
+        if (index>=sizeOfPool):
+            index=0
+        countOfChromosoe= int((choromosoe.fitness*sizeOfPool)/totatFitness)
+        for j in range(0,countOfChromosoe):
+            tempPool.append(choromosoe)
+    return tempPool
+
+
+def CrossOver(sizeOfPool):
+    endOfSelection=sizeOfPool
+    for i in range(0,int(sizeOfPool/2)):
+
+        firstIndex=randint(0,endOfSelection)
+        firstChromosoeTree=pool[firstIndex].tree
+
+        tempChromosoe=pool[endOfSelection-1]
+        pool[endOfSelection-1]=pool[firstIndex]
+        pool[firstIndex]=tempChromosoe
+
+        endOfSelection=endOfSelection-1
+
+        secondIndex=randint(0,endOfSelection)
+        secondChromosoeTree=pool[secondIndex].tree
+        tempChromosoe=pool[endOfSelection-1]
+        pool[endOfSelection-1]=pool[secondIndex]
+        pool[secondIndex]=tempChromosoe
+
+        endOfSelection=endOfSelection-1
+
+
+        firstNodeMaxLevel=0
+        secondNodeMaxLevel=0
+        for node in firstChromosoeTree.nodes:
+            if(node.level>firstNodeMaxLevel):
+                firstNodeMaxLevel=node.level
+        for node in secondChromosoeTree.nodes:
+            if(node.level>secondNodeMaxLevel):
+                secondNodeMaxLevel=node.level
+
+        maxLevelOfSelection=0
+        if (firstNodeMaxLevel<secondNodeMaxLevel):
+            maxLevelOfSelection=firstNodeMaxLevel
+        else:
+            maxLevelOfSelection=secondNodeMaxLevel
+
+        selectedLevel=randint(1,maxLevelOfSelection)
+
+        firstChromosoeSelectedLevelNodes=[x for x in firstChromosoeTree.nodes if x.level==selectedLevel]
+        secondChromosoeSelectedLevelNodes=[x for x in secondChromosoeTree.nodes if x.level==selectedLevel]
+
+        if (len(firstChromosoeSelectedLevelNodes)< len(secondChromosoeSelectedLevelNodes)):
+            selectionNodeIndex=randint(0,len(firstChromosoeSelectedLevelNodes)-1)
+
+        else:
+            selectionNodeIndex=randint(0,len(secondChromosoeSelectedLevelNodes)-1)
+
+        firstNode=firstChromosoeSelectedLevelNodes[selectionNodeIndex]
+        secondNode=secondChromosoeSelectedLevelNodes[selectionNodeIndex]
+
+        firstNodeParent=firstNode.parent
+        secondNodeParent=secondNode.parent
+
+        if (firstNodeParent.rightChildNode==firstNode):
+            firstNodeParent.rightChildNode=secondNode
+            secondNode.parent=firstNodeParent
+        elif (firstNodeParent.leftChildNode==firstNode):
+            firstNodeParent.leftChildNode=secondNode
+            secondNode.parent=firstNodeParent
+
+        if (secondNodeParent.rightChildNode==secondNode):
+            secondNodeParent.rightChildNode=firstNode
+            firstNode.parent=secondNodeParent
+        elif (secondNodeParent.leftChildNode==secondNode):
+            secondNodeParent.leftChildNode=firstNode
+            firstNode.parent=secondNodeParent
+
+
+def Generations(generationCount):
+    for i in range(0,generationCount):
+        for row in parsedArray:
+            dictValue=dict()
+            dictValue[1]=1
+            dictValue[0]=0
+            for i in range(0,5):
+                dictValue['x'+str((5-i))]=int(row[i])
+            CalculateFittnessOFthePool(dictValue,int(row[5]))
+        pool=InitilizePool(60)
+        for chromosoe in pool:
+            chromosoe.fitness=0
+        CrossOver(60)
+
+
+
+operators=['&','|']
+operatorsAndOperands=['&','|','x1','x2','x3','x4','x5']
+operands=['x1','x2','x3','x4','x5']
+pool=[]
+
+
+parsedArray=ParseData('test')
+CreateRandomPool(60)
+"""
+for row in parsedArray:
+    dictValue=dict()
+    dictValue[1]=1
+    dictValue[0]=0
+    for i in range(0,5):
+        dictValue['x'+str((5-i))]=int(row[i])
+    CalculateFittnessOFthePool(dictValue,int(row[5]))
+
+
+print('adad')
+pool=InitilizePool(60)
+for chromosoe in pool:
+    chromosoe.fitness=0
+print('aaaaa')
+CrossOver(60)
+"""
+
+Generations(100)
+
+for i in range(0,60):
+    print(pool[i].fitness)
+
+__author__ = 'Nazemi'
